3bed8fd7849cea66d96fa1952e5e783c86fd7d0e
Sabbir Hosain <contact.sabbirhosain@gmail.com>
2026-06-19 11:46:31 +0600

    update

--- src/controllers/portfolio.controller.js
+++ src/controllers/portfolio.controller.js
@@ -138,7 +138,7 @@ export const show_data = async (req, res) => {
         const searchQuery = new RegExp('.*' + search + '.*', 'i');
         const dataFilter = { status: "active", $or: [{ portfolio_name: { $regex: searchQuery } }] }
 
-        const portfolio = await PortfolioModel.find(dataFilter).populate('categories_id', 'categories_name').sort({ createdAt: -1 }).limit(limit).lean();
+        const portfolio = await PortfolioModel.find(dataFilter).populate('categories_id', 'categories_name').limit(limit).lean();
         const result = portfolio.map((port) => {
             return {
                 ...port,
